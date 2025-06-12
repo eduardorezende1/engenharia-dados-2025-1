1df6e499c2b3ff3fcef26b303c03f62d93f3473e
Eduardo Rezende <erezende771@gmail.com>
2025-06-11 23:03:20 -0300

    Melhora na camada Gold, adicionando nota media por ano, junto com a maior e menor nota do ano

--- dbt_compose/dbt/projeto_dbt/models/marts/agg_movies_by_year.sql
+++ dbt_compose/dbt/projeto_dbt/models/marts/agg_movies_by_year.sql
@@ -1,6 +1,12 @@
 SELECT
     movie_year,
-    COUNT(movie_title) AS total_de_filmes
+    COUNT(movie_title) AS total_de_filmes,
+
+    ROUND(AVG(rating)::numeric, 2) AS nota_media,
+    MIN(rating) AS nota_minima,
+    MAX(rating) AS nota_maxima
+
+
 FROM
     {{ ref('stg_movies') }}
 GROUP BY
